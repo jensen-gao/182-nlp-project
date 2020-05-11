75bad85f0085bcb54090b792dd56ec6340aa136d
Jensen Gao <jensengao@jensens-mbp.attlocal.net>
2020-05-10 17:02:03 -0700

    batched test_submission

--- test_submission.py
+++ test_submission.py
@@ -4,42 +4,54 @@ import tensorflow as tf
 import transformers
 from transformers import DistilBertTokenizer
 from bert_models.tf_distilbert_for_classification import TFDistilBertForClassification
-from bert_models.tf_distilbert_for_ordinal_regression import TFDistilBertForOrdinalRegression
 
 
-class_path = 'models/final_class'
-ord_path = 'models/final_ord'
+model_1_path = 'models/epoch_1'
+model_2_path = 'models/epoch_2'
+model_3_path = 'models/epoch_3'
 
-class_config = transformers.DistilBertConfig.from_pretrained(class_path, num_labels=5)
-ord_config = transformers.DistilBertConfig.from_pretrained(ord_path, num_labels=4)
+model_1_config = transformers.DistilBertConfig.from_pretrained(model_1_path, num_labels=5)
+model_2_config = transformers.DistilBertConfig.from_pretrained(model_2_path, num_labels=5)
+model_3_config = transformers.DistilBertConfig.from_pretrained(model_3_path, num_labels=5)
 class_loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-ord_loss = tf.keras.losses.BinaryCrossentropy(from_logits=True)
-class_model = TFDistilBertForClassification.from_pretrained(class_path, config=class_config)
-ord_model = TFDistilBertForOrdinalRegression.from_pretrained(ord_path, config=ord_config)
-class_model.compile(optimizer='adam', loss=class_loss)
-ord_model.compile(optimizer='adam', loss=ord_loss)
+model_1 = TFDistilBertForClassification.from_pretrained(model_1_path, config=model_1_config)
+model_2 = TFDistilBertForClassification.from_pretrained(model_2_path, config=model_2_config)
+model_3 = TFDistilBertForClassification.from_pretrained(model_3_path, config=model_3_config)
+model_1.compile(optimizer='adam', loss=class_loss)
+model_2.compile(optimizer='adam', loss=class_loss)
+model_3.compile(optimizer='adam', loss=class_loss)
 tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
 
 
 def eval(text):
 	# This is where you call your model to get the number of stars output
-	encoding = tokenizer.encode_plus(text, max_length=384)
-	input_ids = encoding['input_ids']
-	input_ids = np.expand_dims(input_ids, 0)
-	class_prediction = class_model.predict_on_batch(input_ids)
-	ord_prediction = ord_model.predict_on_batch(input_ids)
-	class_stars = np.argmax(class_prediction) + 1
-	ord_stars = np.sum(ord_prediction > 0) + 1
-	return int(np.round((class_stars + ord_stars) / 2))
-
+	encoding = tokenizer.batch_encode_plus(text, max_length=384, pad_to_max_length=True, return_attention_masks=True)
+	input_ids = np.array(encoding['input_ids'])
+	attention_masks = np.array(encoding['attention_mask'])
+	prediction_1 = model_1.predict_on_batch([input_ids, attention_masks])
+	prediction_2 = model_2.predict_on_batch(input_ids)
+	prediction_3 = model_3.predict_on_batch(input_ids)
+	stars_1 = np.argmax(prediction_1, axis=1) + 1
+	stars_2 = np.argmax(prediction_2, axis=1) + 1
+	stars_3 = np.argmax(prediction_3, axis=1) + 1
+	return np.round(stars_1 + stars_2 + stars_3) / 3
 
 if len(sys.argv) > 1:
 	validation_file = sys.argv[1]
 	with open("output.jsonl", "w") as fw:
 		with open(validation_file, "r") as fr:
+			review_ids = []
+			text = []
 			for line in fr:
 				review = json.loads(line)
-				fw.write(json.dumps({"review_id": review['review_id'], "predicted_stars": eval(review['text'])})+"\n")
+				review_ids.append(review['review_id'])
+				text.append(review['text'])
+				if len(review_ids) >= 16:
+					results = eval(text)
+					for i in range(len(review_ids)):
+						fw.write(json.dumps({"review_id": review_ids[i], "predicted_stars": int(results[i])})+"\n")
+					review_ids = []
+					text = []
 	print("Output prediction file written")
 else:
 	print("No validation file given")
