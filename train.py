9ed473ab3b16c80eb2f4f0a89ba70a7332fae8aa
Kevin Li <kevintli98@gmail.com>
2020-05-11 04:17:35 -0700

    Add comments to data preprocessing code
    Add star visualization and weighted loss
    
    Add args option for layer norm
    
    Allow custom max length and batch size

--- train.py
+++ train.py
@@ -18,18 +18,63 @@ parser.add_argument('--ordinal', '-o', action='store_true',
                     help='Whether to use ordinal regression instead of classification.')
 parser.add_argument('--as_features', '-f', action='store_true',
                     help='Whether to freeze the BERT layers and use them only as features instead of fine-tuning.')
+parser.add_argument('--loss_weights', '-lw', nargs='+', default=[1, 1, 1, 1, 1],
+                    help='Loss weights for each possible star label')
+parser.add_argument('--layer_norm', '-n', action='store_true',
+                    help='Whether to use layer normalization before the classification output')
+parser.add_argument('--batch_size', '-b', type=int, default=16,
+                    help='Batch size to use for training')
 args = parser.parse_args()
 
 strategy = tf.distribute.MirroredStrategy()
 
+def make_weighted_loss(loss_type, weights, ord=False):
+    """
+    Returns a weighted version of `loss_fn`.
+
+    Parameters
+    ==========
+    loss_type : Class[tf.keras.losses.Loss]
+        The class of the original loss function
+
+    weights : list
+        A list of weights, one for each possible label.
+
+    ord : bool
+        If true, assumes that the labels are ordinal labels.
+    """
+    weights = tf.constant(weights)
+    loss_fn = loss_type(from_logits=True, reduction=tf.keras.losses.Reduction.NONE)
+
+    def loss(y_true, y_pred):
+        indices = tf.cast(tf.reduce_sum(y_true, axis=1) if ord else tf.constant(y_true), tf.int32)
+        losses = loss_fn(y_true, y_pred)
+        weighted_losses = tf.gather(weights, indices) * losses
+        return tf.reduce_mean(weighted_losses)
+    
+    return loss
+
+assert len(args.loss_weights) == 5, "Must have exactly 5 loss weights (one for each star rating)"
+loss_weights = [float(i) for i in args.loss_weights]
+weighted_loss = (loss_weights != [1, 1, 1, 1, 1])
+
+SparseCategoricalCrossentropy = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+WeightedSparseCategoricalCrossentropy = make_weighted_loss(tf.keras.losses.SparseCategoricalCrossentropy, loss_weights, ord=False)
+
+BinaryCrossentropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+WeightedBinaryCrossentropy = make_weighted_loss(tf.keras.losses.BinaryCrossentropy, loss_weights, ord=True)
+
+if weighted_loss:
+    print(f"Using loss weights {loss_weights}")
+
 if args.ordinal:
     model_type = TFDistilBertForOrdinalRegression
-    loss = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+    loss = WeightedBinaryCrossentropy if weighted_loss else BinaryCrossentropy
     num_labels = 4
     metrics = [ord_pred_accuracy, ord_pred_abs_error]
 else:
     model_type = TFDistilBertForClassification
-    loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    loss = WeightedSparseCategoricalCrossentropy if weighted_loss else SparseCategoricalCrossentropy
     num_labels = 5
     metrics = ['accuracy', pred_abs_error]
 
@@ -40,14 +85,14 @@ else:
 
 with strategy.scope():
     if args.pretrain:
-        model = model_type.from_pretrained('pretrained/', config=config, as_features=args.as_features, from_pt=True)
+        model = model_type.from_pretrained('pretrained/', config=config, as_features=args.as_features, use_layer_norm=args.layer_norm, from_pt=True)
     else:
-        model = model_type.from_pretrained('distilbert-base-uncased', config=config, as_features=args.as_features)
+        model = model_type.from_pretrained('distilbert-base-uncased', config=config, as_features=args.as_features, use_layer_norm=args.layer_norm)
     model.compile(optimizer='adam', loss=loss, metrics=metrics)
 
 
 epochs = 4
-batch_size_per_replica = 16
+batch_size_per_replica = args.batch_size
 batch_size = batch_size_per_replica * strategy.num_replicas_in_sync
 
 train_dataset = load_data(split='train', ordinal=args.ordinal, batch_size=batch_size)
